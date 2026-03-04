9f4d723e0747b59b77495182bb0630c5d41db438
ThoraDis <142800101+ThoraDis@users.noreply.github.com>
2026-03-04 18:21:19 +0000

    image api

--- src/api/image.api.ts
+++ src/api/image.api.ts
@@ -6,23 +6,101 @@ import {pagingSchema, createImageSchema,updateImageSchema} from "../schema.zod.j
 export const app = new Hono();
 
 //ná í 
-app.get('/',zValidator('query',pagingSchema) ,async(c)=>{})
+app.get('/',zValidator('query',pagingSchema) ,async(c)=>{
+        const limit=c.req.valid('query').limit
+    const offset =c.req.valid('query').offset
+
+    const image = await prisma.image.findMany({skip:offset, take:limit});
+
+    const imageCount = await prisma.image.count()
+
+    const response = {
+        data: image,
+        paging: {
+            limit,
+            offset,
+            count: imageCount
+            }
+        }
+
+    return c.json(response,200)
+
+})
 
 //Ná í eftir id eða slug
-app.get('/:id',zValidator('query',pagingSchema) ,async(c)=>{})
+app.get('/:id',zValidator('query',pagingSchema) ,async(c)=>{
+    const id = c.req.param('id')
+
+    const image = await prisma.image.findUnique({
+        where: { id: Number(id) },
+    });
+
+    if (!image) {
+        return c.json({ error: 'no such image' }, 404);
+    }
+
+    const response = {
+        data: image
+    }
+
+    return c.json(response,200)
+
+})
 
 //Búa til
 app.post('/',zValidator('query',createImageSchema,(result, c) => { if (!result.success) {
-      return c.json("Bad request",400)}}), async(c)=>{})
+      return c.json("Bad request",400)}}), async(c)=>{
+        const image=c.req.valid('query').image
+        const eventId =c.req.valid('query').eventId
+
+        const newImage = await prisma.image.create({
+            data:{
+                image:image,
+                eventId:eventId
+            }
+        })
+
+        const response = {
+            data: newImage,
+
+        }
+
+        return c.json(response,201)
+      })
 
 
 //Uppfæra
-app.put('/:id',zValidator('query',updateImageSchema,(result, c) => {
-    if (!result.success) {
-      return c.json("Bad request",400)
-    }
-  }), async(c)=>{})
+app.put('/:id',zValidator('query',updateImageSchema,(result, c) => {if (!result.success) {
+      return c.json("Bad request",400)}}), async(c)=>{
+        const id = c.req.param('id')
+        const image=c.req.valid('query').image
+        const eventId =c.req.valid('query').eventId
+
+        const updatedImage=await prisma.image.update({
+            where: {id:Number(id),},
+                data:{
+                image:image,
+                eventId:eventId
+                },
+            
+            });
+
+        const response = {
+            data: updatedImage
+        }
+
+        return c.json(response,200)
+
+      })
     
 
 //Eyða
-app.delete('/:id',zValidator('query',pagingSchema) ,async(c)=>{})
+app.delete('/:id',zValidator('query',pagingSchema) ,async(c)=>{
+    const id = c.req.param('id')
+
+    await prisma.event.delete({
+    where: {
+        id:Number(id),},});
+
+    return c.json(204)
+})
