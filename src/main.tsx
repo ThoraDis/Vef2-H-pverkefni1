5fea5c0dae7ad452c223bd41c3ca1589c2595a33
Johann Chanse Sigurdsson <jcs3@hi.is>
2026-03-05 16:07:46 +0000

    first pass on cloudinary 'getting started' instructions

--- src/main.tsx
+++ src/main.tsx
@@ -8,6 +8,58 @@ import z from "zod";
 import { ErrorPage } from "./components/ErrorPage.js";
 // búum til og exportum Hono app
 export const app = new Hono();
+//cloudinary leiðbeiningar: 
+// - cloudinary --> image --> getting started
+// - https://www.youtube.com/watch?v=2Z1oKtxleb4
+import { v2 as cloudinary } from 'cloudinary';
+
+(async function() {
+
+    // Configuration
+    cloudinary.config({ 
+        cloud_name: 'dsfjdk2rt', 
+        api_key: '817145963542165', 
+        api_secret: '<your_api_secret>' // Click 'View API Keys' above to copy your API secret
+    });
+    
+    // Upload an image
+     const uploadResult = await cloudinary.uploader
+       .upload(
+           'https://res.cloudinary.com/demo/image/upload/getting-started/shoes.jpg', {
+               public_id: 'shoes',
+           }
+       )
+       .catch((error) => {
+           console.log(error);
+       });
+    
+    console.log(uploadResult);
+    
+    // Optimize delivery by resizing and applying auto-format and auto-quality
+    const optimizeUrl = cloudinary.url('shoes', {
+        fetch_format: 'auto',
+        quality: 'auto'
+    });
+    
+    console.log(optimizeUrl);
+    
+    // Transform the image: auto-crop to square aspect_ratio
+    const autoCropUrl = cloudinary.url('shoes', {
+        crop: 'auto',
+        gravity: 'auto',
+        width: 500,
+        height: 500,
+    });
+    
+    console.log(autoCropUrl);    
+})();
+
+//TODO: 
+/*vantar fyrir cloudinary:
+1. hlaða upp myndir. app.post('/upload')
+2. ná myndir app.get('/images/:id')
+*/
+
 
 // sendir út allt sem er í static möppunni
 app.use('/*', serveStatic({ root: './static' }));
