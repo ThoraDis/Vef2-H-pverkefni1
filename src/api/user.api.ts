792bb7752a949f6aeecee26fa4defed84a6b3568
valurkristinn <109678364+valurkristinn@users.noreply.github.com>
2026-03-05 14:52:03 +0000

    added user api and an idSchema to the schema file

--- src/api/user.api.ts
+++ src/api/user.api.ts
@@ -1,28 +1,111 @@
 import { Hono } from "hono";
-import { prisma } from '../prisma.js'
-import {zValidator} from '@hono/zod-validator'
-import {pagingSchema,createUserSchema,updateUserSchema} from "../schema.zod.js"
+import { prisma } from "../prisma.js";
+import { zValidator } from "@hono/zod-validator";
+import {
+  pagingSchema,
+  createUserSchema,
+  updateUserSchema,
+  idSchema,
+} from "../schema.zod.js";
 
 export const app = new Hono();
 
-//ná í 
-app.get('/',zValidator('query',pagingSchema) ,async(c)=>{})
+//ná í
+app.get("/", zValidator("query", pagingSchema), async (c) => {
+  const limit = c.req.valid("query").limit;
+  const offset = c.req.valid("query").offset;
+
+  const users = await prisma.user.findMany({ skip: offset, take: limit });
+
+  const usersCount = await prisma.user.count();
+
+  const response = {
+    data: users,
+    paging: {
+      limit,
+      offset,
+      count: usersCount,
+    },
+  };
+
+  return c.json(response, 200);
+});
 
 //Ná í eftir id eða slug
-app.get('/:id',zValidator('query',pagingSchema) ,async(c)=>{})
+app.get("/:id", zValidator("param", idSchema), async (c) => {
+  const id = c.req.valid("param").id;
+
+  const user = await prisma.user.findUnique({ where: { id: id } });
+
+  if (!user) {
+    return c.json({ error: "no such user" }, 404);
+  }
+
+  return c.json(user, 200);
+});
 
 //Búa til
-app.post('/',zValidator('query',createUserSchema,(result, c) => { if (!result.success) {
-      return c.json("Bad request",400)}}), async(c)=>{})
+app.post(
+  "/",
+  zValidator("form", createUserSchema, (result, c) => {
+    if (!result.success) {
+      return c.json("Bad request", 400);
+    }
+  }),
+  async (c) => {
+    const email = c.req.valid("form").email;
 
+    const newUser = await prisma.user.create({
+      data: {
+        email: email,
+      },
+    });
+
+    const response = {
+      data: newUser,
+    };
+
+    return c.json(response, 201);
+  },
+);
 
 //Uppfæra
-app.put('/:id',zValidator('query',updateUserSchema,(result, c) => {
+app.put(
+  "/:id",
+  zValidator("form", updateUserSchema, (result, c) => {
     if (!result.success) {
-      return c.json("Bad request",400)
+      return c.json("Bad request", 400);
     }
-  }), async(c)=>{})
-    
+  }),
+  zValidator("param", idSchema),
+  async (c) => {
+    const id = c.req.valid("param").id;
+    const email = c.req.valid("form").email;
+
+    const newUser = await prisma.user.update({
+      where: { id: id },
+      data: {
+        email: email,
+      },
+    });
+
+    const response = {
+      data: newUser,
+    };
+
+    return c.json(response, 201);
+  },
+);
 
 //Eyða
-app.delete('/:id',zValidator('query',pagingSchema) ,async(c)=>{})
+app.delete("/:id", zValidator("param", idSchema), async (c) => {
+  const id = c.req.valid("param").id;
+
+  await prisma.user.delete({
+    where: {
+      id: id,
+    },
+  });
+
+  return c.json(204);
+});
