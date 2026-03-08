cb03942cf0801737ebd92814b583bfcd1ae06bb0
valurkristinn <109678364+valurkristinn@users.noreply.github.com>
2026-03-08 21:54:31 +0000

    fixed auth,
    completely removed jwt and only using betterauth, kept the file as
    jwtauth.ts to keep consistency over the commit history

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,6 @@
 import "dotenv/config";
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
-import { jwt } from "better-auth/plugins";
 import { prisma } from "../db/client.js";
 
 export const auth = betterAuth({
@@ -9,13 +8,16 @@ export const auth = betterAuth({
   baseURL: process.env.BETTER_AUTH_URL, // e.g. http://localhost:4000
   database: prismaAdapter(prisma, { provider: "postgresql" }),
 
-  trustedOrigins: ["http://localhost:3000"],
+  trustedOrigins: [
+    process.env.BETTER_AUTH_SECRET
+      ? process.env.BETTER_AUTH_SECRET
+      : "http://localhost:3000",
+  ],
 
   emailAndPassword: {
     enabled: true,
   },
 
-  plugins: [jwt()],
 });
 
 export type AuthType = {
