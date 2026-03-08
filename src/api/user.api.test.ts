9b4097a7df17528a07e81fbefad01ab6dd48b186
valurkristinn <109678364+valurkristinn@users.noreply.github.com>
2026-03-08 23:42:41 +0000

    again!!

--- src/api/user.api.test.ts
+++ src/api/user.api.test.ts
@@ -1,6 +1,6 @@
 import assert from "node:assert";
 import { describe, it } from "node:test";
-import { prisma } from "../generated/prisma/index.js";
+import { prisma } from "../../../generated/prisma/index.js";
 import { after } from "node:test";
 
 after(async () => {
