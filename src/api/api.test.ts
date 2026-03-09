9274c8c6a491ed9b22509f6d5567068adba6ae89
valurkristinn <109678364+valurkristinn@users.noreply.github.com>
2026-03-09 02:41:57 +0000

    yupyup

--- src/api/api.test.ts
+++ src/api/api.test.ts
@@ -1,6 +1,6 @@
 import assert from "node:assert";
 import { describe, it, before } from "node:test";
-import { PrismaClient } from "../generated/prisma/client.ts";
+import { PrismaClient } from "../generated/prisma/client.js";
 import { PrismaPg } from "@prisma/adapter-pg";
 
 const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
