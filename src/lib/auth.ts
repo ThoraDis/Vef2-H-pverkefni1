f23b4636111e3a8efde84cdd2e1ed6b2914faf3b
valurkristinn <109678364+valurkristinn@users.noreply.github.com>
2026-04-17 18:14:15 +0000

    anotha one

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -8,7 +8,7 @@ export const auth = betterAuth({
   baseURL: process.env.BETTER_AUTH_URL, // e.g. http://localhost:4000
   database: prismaAdapter(prisma, { provider: "postgresql" }),
 
-  trustedOrigins: ["http://localhost:3000"],
+  trustedOrigins: ["http://localhost:3000", "https://vef2-2026-h2-git-dagur-valurks-projects.vercel.app", "vef2-2026-h2.vercel.app"],
 
   advanced: {
     useSecureCookies: true,
